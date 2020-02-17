6a33cb286d0a4839ef929e84d1ab946527d3e95d
auksasful <akurat2012@gmail.com>
2020-02-17 20:08:40 +0200

    Projekto solution įkėlimas

---
+++ Semestro_projektas/Semestro_projektas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Semestro_projektas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
